ab0c109418482c7c359d717a63159dc2dc760690
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-02 23:54:35 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
