881b0b1ed514cde30b8cd7963d773b9db4cd43e4
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-02 23:49:44 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints a quote
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
