8d5a3d55295a8e830516589d42ae4a3c1a5db872
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-05 18:09:03 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,29 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints two combination and ignores
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
