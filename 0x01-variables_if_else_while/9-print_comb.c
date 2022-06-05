ee4f8123f0f7ceae9bccdbd079a0a4389364c1d3
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-05 15:26:58 +0100

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
-#!?bin/bash
+#include <stdio.h>
+/**
+ * main -  prints all possible combinations of single-digit numbers.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
