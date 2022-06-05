e64ba171a42ffe6133e36b6d39b2f4ed51ed3191
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-05 15:12:12 +0100

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints all alphabets in lowercase,
+ * followed by a new line
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
