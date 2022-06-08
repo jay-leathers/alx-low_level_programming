fb1fa37b87cdbb0f52ee665a7c9934b3d17242f6
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-08 04:00:12 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-khurdijat
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
