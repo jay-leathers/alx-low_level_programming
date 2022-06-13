1ab930c83ec1e04c5e054632077d0d8b61273faa
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-13 13:54:30 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,10 @@
-stairs
+#include "main.h"
+/**
+* reset_to_98 - updates the value it points
+*@n: pointer of the direction at the variable n
+*Return: 0 (success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
