aac8bbf81a6b95ff890acf3664b66c87ac47bb8a
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-22 19:09:00 +0100

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,17 +1,15 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _puts_recursion - main - check the code
- * @s: character of string
- * Return: Always 0.
- */
-void _puts_recursion(char *s)
-{
-	if (*(s) != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	if (*(s) == '\0')
-		_putchar('\n');
-}
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif /*MAIN_H*/
