6f717ffff29fcd0e2f58962a0511fb45d1847eeb
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-06-24 21:43:38 +0100

    0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *  * _strcat -  a function that concatenates two strings.
+ *   * @src: first parameter.
+ *    * @dest: second parameter.
+ *     * Return: 0
+ *      */
+
+char *_strcat(char *dest, char *src)
+{
+		return (strcat(dest, src));
+}
